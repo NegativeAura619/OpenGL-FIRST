4d9eb20e977306a8a909a67506321628931b2c4f
konrad capanda <lolmidgod@outlook.com>
2024-03-10 18:57:29 +0100

    shadery plus shaderpgoram

--- FirstOpenGL/Main.cpp
+++ FirstOpenGL/Main.cpp
@@ -2,6 +2,21 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main()
 {
 
@@ -12,6 +27,16 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // wybor profilu openGL
 
+
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // left corner
+		 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // right corner 
+		 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // top corner
+	};
+
+
+
 	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGl", NULL, NULL); // parametry okienka  szerokosc,wysokosc,nazwa,fullscreen i ?
 	if (window == NULL) 
 	{
@@ -21,11 +46,31 @@ int main()
 	}
 	glfwMakeContextCurrent(window); // wymusza uzywanie okienka ktore zainicjowalismy
 
-	gladLoadGL();
+	gladLoadGL(); // ladowanie glada by konfigurowac opengl
 
 
 
 	glViewport(0,0, 800,800); //koordynaty obszaru ktory chcemy wyswietlac
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER); //tworzymy shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // przypisujemy parametry
+	glCompileShader(vertexShader); // kompilacja shadera
+
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); // To samo co wy¿ej tylko z fragmentami
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader); 
+
+	GLuint shaderProgram = glCreateProgram(); //tworzenie shader programu do uzycia shaderow
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader); // dodawanie shaderow do Shader programu 
+
+	glLinkProgram(shaderProgram); // wrapowanie shader programu
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader); // usuwanie shaderow poniewa¿ s¹ juz w programie 
+
 
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // kolor nastepnej klatki 
 	glClear(GL_COLOR_BUFFER_BIT); //inicjalizacja koloru
