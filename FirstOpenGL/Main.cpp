0f63408fec974d9a307224515f0528fa1acc6c73
konrad capanda <lolmidgod@outlook.com>
2024-03-11 20:30:13 +0100

    Organizowanie shaderow w osobnych plikach dodano reader, trzeba duzo naprawić

--- FirstOpenGL/Main.cpp
+++ FirstOpenGL/Main.cpp
@@ -2,21 +2,6 @@
 #include <glad/glad.h> // glad musi byc zalinkowany przed GLFW	
 #include <GLFW/glfw3.h>
 
-// Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\n\0";
-
 int main()
 {  
 
@@ -132,7 +117,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 9 , GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 9 , GL_UNSIGNED_INT, 0); // przy 1 trojkacie dodajemy glDrawArrays(GL_TRIANGLES,0,3);
 		glfwSwapBuffers(window); // updatujemy obraz co klatke
 		
 
